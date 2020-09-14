8a358071051585642b7f733f82667ee082bcd5cd
Christina Howard <clhoward33@icloud.com>
2020-09-14 19:48:38 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -7,12 +7,12 @@ require 'pry'
        email: "jon_snow@thewall.we",
        favorite_ice_cream_flavors: ["chocolate", "vanilla"]
      },
-  #   "Freddy Mercury" => {
-  #     name: "Freddy",
-  #     email: "freddy@mercury.com",
-  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-  #   }
-  # }
+     "Freddy Mercury" => {
+       name: "Freddy",
+       email: "freddy@mercury.com",
+       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+     }
+   }
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
